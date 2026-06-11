eef2a7c516688d89c14955475612115400a9adad
juliocesar-96 <julio_cbs96@hotmail.com>
2026-06-11 14:46:27 -0300

    mudando a url par anova

--- omlpi-www/config.js
+++ omlpi-www/config.js
@@ -1,28 +1,29 @@
 const productionDomains = [
-    'tmp--omlpi.appcivico.com',
+    'rnpiobserva.org.br',
+    'www.rnpiobserva.org.br' // Recomendado incluir com www caso os usuários acessem assim
 ];
 
 export default {
     api: {
         domain: (productionDomains.indexOf(window.location.hostname) > -1
-            ? 'http://tmp--omlpi-api.appcivico.com/v2/'
+            ? 'https://omlpi-api.rnpiobserva.org.br/v2/' // Mantemos o /v2/ que a API própria exige
             : ''
         ),
         docs: (productionDomains.indexOf(window.location.hostname) > -1
-            ? 'http://tmp--omlpi-docs.appcivico.com/'
+            ? 'https://docs.rnpiobserva.org.br/' // Conforme seu primeiro relato, esse é o subdomínio dos docs
             : '/'
         ),
     },
     apiCMS: {
         domain: (productionDomains.indexOf(window.location.hostname) > -1
-            ? 'http://tmp--omlpi-strapi.appcivico.com/'
-            : 'http://localhost:1243/'
+            ? 'https://omlpi-strapi.rnpiobserva.org.br/'
+            : 'http://localhost:1243/' // Mantido o padrão original para desenvolvimento local
         ),
     },
     storage: {
         domain: (productionDomains.indexOf(window.location.hostname) > -1
-            ? 'http://tmp--omlpi-strapi.appcivico.com/'
-            : 'https://tmp--omlpi-strapi.appcivico.com/'
+            ? 'https://omlpi-strapi.rnpiobserva.org.br/'
+            : 'https://omlpi-strapi.rnpiobserva.org.br/' // Recomendado apontar para o seu Strapi aqui também, para que as imagens carreguem se você rodar o site localmente
         ),
     },
     firstCityId: 5200050,
